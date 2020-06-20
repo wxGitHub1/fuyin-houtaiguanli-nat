585ca1adbbaa544b50f840de2d914a78e7c3edda
wxGitHub1 <37536274+wxGitHub1@users.noreply.github.com>
2020-06-20 17:37:29 +0800

    上线版本

--- src/api/pathUrl.js
+++ src/api/pathUrl.js
@@ -1,4 +1,5 @@
-const pathUrl_net = 'http://117.34.105.87:88';
+const pathUrl_net = 'http://117.34.105.87:88';//线上地址
+// const pathUrl_net = 'http://192.168.1.167:86';//测试地址
 
 export default {
     pathUrl_net
